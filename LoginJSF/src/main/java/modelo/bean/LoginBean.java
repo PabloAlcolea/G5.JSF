f1f884db70f0956a8ea20a39109a8e922d541929
Palco <Palco@10.245.20.12>
2024-11-06 17:40:33 +0100

    Push lab

---
+++ LoginJSF/src/main/java/modelo/bean/LoginBean.java
@@ -0,0 +1,34 @@
+package modelo.bean;
+import java.io.Serializable;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+@Named("login")
+@SessionScoped
+public class LoginBean implements Serializable {
+private static final long serialVersionUID = 1L;
+private String nombre;
+private String password;
+
+public String getNombre() {
+ return nombre;
+ }
+
+public void setNombre(String nombre) {
+ this.nombre = nombre;
+ }
+public String getPassword() {
+ return password;
+ }
+public void setPassword(String password) {
+ this.password = password;
+ }
+
+public String comprobar() {
+ if(nombre.equals("pirata")){
+ return "error";
+ }
+ else {
+ return "ok";
+ }
+ }
+} 
